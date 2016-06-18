4b7fba29b6fa8a58ed42b9c70024dc0ecd639956
mao <myj226@gmail.com>
2016-06-18 16:21:30 +0800

    Add colourlovers.com api's color patterns for kit

--- src/components/DrumKit/index.js
+++ src/components/DrumKit/index.js
@@ -8,12 +8,13 @@ class DrumKit extends Component {
   render() {
     const { sources, actions } = this.props
     const columns = ['_cymbal', '_hihat', '_snare', '_bass', '_tom']
+
     return (
       <div className={style.main}>
-        {columns.map(c => 
-          <div className={classnames({[style.block]: true, [style['block-'+c]]:true})}>
+        {columns.map((c, index) =>
+          <div className={classnames({[style.block]: true})} style={{backgroundColor: '#'+colorPatterns[randomIndex][index]}}>
             {sources.map(source =>
-              source.type === c ? 
+              source.type === c ?
               <div className={classnames({[style.block]: true})} onClick={(el) => actions.playSound(source.title)}>
                 <span>{source.title.slice(1)}</span>
                 <GoUnmute className={classnames({[style.icon]: true, [style.playing]: source.playing})} />
@@ -26,5 +27,23 @@ class DrumKit extends Component {
     )
   }
 }
+
+const colorPatterns = [
+  [ "FCEBB6", "5E412F", "F07818", "78C0A8", "F0A830" ],
+  [ "F8F4D7", "F4DEC2", "F4B36C", "E98977", "F2B4A8" ],
+  [ "ECD078", "D95B43", "C02942", "542437", "53777A" ],
+  [ "FFFFFF", "F2EFEB", "FAF5ED", "EDE9E4", "F0ECE9" ],
+  [ "F9CDAD", "FE4365", "FC9D9A", "83AF9B", "C8C8A9" ],
+  [ "027B7F", "FFA588", "D62957", "BF1E62", "572E4F" ],
+  [ "E7E4D5", "C84648", "FFF3DB", "D3C8B4", "703E3B" ],
+  [ "E8D3A9", "E39B7D", "6E6460", "89B399", "BCBFA3" ],
+  [ "BBC793", "77CCA4", "827551", "F1EEC5", "FECC99" ],
+  [ "EFD88B", "FE4D83", "10C4C0", "E5D3BB", "433B50" ],
+  [ "D07E0E", "E9DEB0", "2F615E", "482C21", "A73E2B" ],
+  [ "2A0308", "924F1B", "E2AC3F", "F8EDC6", "7BA58D" ],
+  [ "140D1A", "42142A", "FF2E5F", "FFD452", "FAEECA" ],
+  [ "EAC388", "DD423E", "3F2C26", "A2A384", "C5AD4B" ]
+]
+const randomIndex = Math.floor(Math.random() * colorPatterns.length + 1) + 1
 
 export default DrumKit
